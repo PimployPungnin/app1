1f8b9a7573b1386a18525ce458823e4ff59d19b8
PimployPungnin <tangmo00pp@gmail.com>
2023-08-18 14:36:47 +0700

    Update banner.js

--- src/banner.js
+++ src/banner.js
@@ -8,8 +8,7 @@ export default function Banner(){
     const r = randInt(1,3)
     let banner = ''
     let url = ''
-}
-
+    
 switch(r) {
     case 1:
         banner = './banners/react.jpg'
@@ -23,16 +22,17 @@ switch(r) {
         banner = './banners/node.jpg'
         url = 'http://www.nodejs.org'
         break
-    default: break
+        default: break
+    }
+    
+    const bannerClick = () => {
+        window.open(url)
+    }
+    
+    return (
+        <div style={{margin: '20px', textAlign: 'center'}}>
+            <img src={banner} onClick={bannerClick} style={{cursor:'pointer'}}
+            alt="banner"/>
+        </div>
+    )
 }
-
-const bannerClick = () => {
-    window.open(url)
-}
-
-return (
-    <div style={{margin: '20px', textAlign: 'center'}}>
-        <img src={banner} onClick={bannerClick} style={{cursor:'pointer'}}
-        alt="banner"/>
-    </div>
-)
