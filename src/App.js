8eff0a8544b1a87c405a3f5256871ec5e6678398
PimployPungnin <tangmo00pp@gmail.com>
2023-08-18 14:36:59 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import React from 'react'
-import './style.css'
-
-function App() {
-    return ( <
-        >
-        <
-        h2 > DeveloperThai.com < /h2> <
-        div className = 'title' > React & React Native < /div> <
-        />
-    )
+import { Calculator2 } from './calculator'
+export default function App() {
+    return <Calculator2 />
 }
-
-export default App;
