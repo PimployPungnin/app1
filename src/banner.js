fbd4aa80bfb88fdb314bb9f500e50d524bf834c4
PimployPungnin <tangmo00pp@gmail.com>
2023-08-18 14:02:27 +0700

    add pic

---
+++ src/banner.js
@@ -0,0 +1,38 @@
+import React from 'react'
+function randInt(min,max){
+    let x = (max-min) + 1
+    return min + Math.floor(Math.random()*x)
+}
+
+export default function Banner(){
+    const r = randInt(1,3)
+    let banner = ''
+    let url = ''
+}
+
+switch(r) {
+    case 1:
+        banner = './banners/react.jpg'
+        url = 'http://www.reactjs.org'
+        break
+    case 2:
+        banner = './banners/react-native.jpg'
+        url = 'http://www.react-native.com'
+        break
+    case 3:
+        banner = './banners/node.jpg'
+        url = 'http://www.nodejs.org'
+        break
+    default: break
+}
+
+const bannerClick = () => {
+    window.open(url)
+}
+
+return (
+    <div style={{margin: '20px', textAlign: 'center'}}>
+        <img src={banner} onClick={bannerClick} style={{cursor:'pointer'}}
+        alt="banner"/>
+    </div>
+)
