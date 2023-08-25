257c529094a8f6b9f657cbbce471f44039668595
PimployPungnin <tangmo00pp@gmail.com>
2023-08-25 13:42:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { EventData2 } from './event-data'
+import RefsFunc from './refs-func'
 export default function App() {
-    return <EventData2/>
+    return <RefsFunc />
 }
