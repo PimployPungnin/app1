e0d8b5e8212d05ed1935aaef1f2e7fdaa1e98cb3
PimployPungnin <tangmo00pp@gmail.com>
2023-08-25 14:59:00 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import RefsArray from './refs-array'
+import MessageBox from './state-func'
 export default function App() {
-    return <RefsArray />
+    return <MessageBox />
 }
