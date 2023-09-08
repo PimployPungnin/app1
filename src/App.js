34dba4f9644f805142e946e57b1db14f8660049e
PimployPungnin <tangmo00pp@gmail.com>
2023-09-08 14:31:09 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from 'react'
-import MessageBox from './state-func'
-export default function App() {
-    return <MessageBox />
+import React from "react";
+import { userContext } from "./context";
+import Content from "./context-content";
+
+export default function App(){
+    return(
+        <userContext.Provider value={'Tom Jerry'}>
+            <Content/>
+        </userContext.Provider>
+    )
 }
