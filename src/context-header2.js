2c500d7f7c5e186e3e2aeeb1f318a6366f5be603
PimployPungnin <tangmo00pp@gmail.com>
2023-09-08 15:17:12 +0700

    Create context-header2.js

---
+++ src/context-header2.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { userContext } from "./context";
+
+export default class Header extends React.Component{
+    static contextType = userContext
+
+    render(){
+        let[user,setUser] = this.context
+        const headerStyle ={
+            backgroundColor:'#cee',
+            textAlign:'center',
+            padding: 5
+        }
+        const onClickSignout = (event) =>{
+            event.preventDefault()
+            setUser('')
+        }
+        const onClickSigin = (event) =>{
+            event.preventDefault()
+            setUser('Hello Tom Jerry')
+        }
+        
+        return(
+            <div style={headerStyle}>
+                <a href=" ">Home</a>&nbsp;-&nbsp; 
+                <a href=" ">Product</a>&nbsp;-&nbsp; 
+                <a href=" ">Contact Us</a>&nbsp;-&nbsp;&nbsp;
+                {
+                    (user)
+                    ?<span>[{user}&nbsp;:&nbsp;<a href=" "
+                        onClick={onClickSignout}>Signout</a>]</span>
+                    :<span>[<a href=" " 
+                        onClick={onClickSigin}>Signin</a>]</span>
+                }
+            </div>
+        )
+    }
+}
