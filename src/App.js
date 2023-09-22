d1936a1e507d0b8d1ed54fbd72c4bf84f627c4a4
PimployPungnin <tangmo00pp@gmail.com>
2023-09-22 15:04:54 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
-import React from 'react'
-import { userContext } from './context'
-import header2 from './context-header2'
-import content2 from './context-content2'
+import Router3 from "./router3";
 export default function App() {
-    let [user,setUser] = React.useState('')
-    return (
-        <userContext.Provider value={[user, setUser]}>
-        <header/>
-        <content/>
-        </userContext.Provider>
-    )
+    return <Router3 />
 }
